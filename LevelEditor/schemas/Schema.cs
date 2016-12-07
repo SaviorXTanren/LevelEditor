ad5d3942faa9a94fbf7068432660847265fc4054
Matthew Olivo <saviorxtanren@gmail.com>
2016-12-06 22:43:14 -0800

    Updating SXT Physics to be a GameObjectGroup

--- LevelEditor/schemas/Schema.cs
+++ LevelEditor/schemas/Schema.cs
@@ -191,8 +191,33 @@ namespace LevelEditor
             sxtState.lockedAttribute = sxtState.Type.GetAttributeInfo("locked");
             sxtState.transformationTypeAttribute = sxtState.Type.GetAttributeInfo("transformationType");
             sxtState.componentChild = sxtState.Type.GetChildInfo("component");
-            sxtState.resourceChild = sxtState.Type.GetChildInfo("resource");
-            sxtState.stmRefChild = sxtState.Type.GetChildInfo("stmRef");
+
+            sxtPhysics.Type = getNodeType("gap", "sxtPhysics");
+            sxtPhysics.transformAttribute = sxtPhysics.Type.GetAttributeInfo("transform");
+            sxtPhysics.translateAttribute = sxtPhysics.Type.GetAttributeInfo("translate");
+            sxtPhysics.rotateAttribute = sxtPhysics.Type.GetAttributeInfo("rotate");
+            sxtPhysics.scaleAttribute = sxtPhysics.Type.GetAttributeInfo("scale");
+            sxtPhysics.pivotAttribute = sxtPhysics.Type.GetAttributeInfo("pivot");
+            sxtPhysics.nameAttribute = sxtPhysics.Type.GetAttributeInfo("name");
+            sxtPhysics.visibleAttribute = sxtPhysics.Type.GetAttributeInfo("visible");
+            sxtPhysics.lockedAttribute = sxtPhysics.Type.GetAttributeInfo("locked");
+            sxtPhysics.transformationTypeAttribute = sxtPhysics.Type.GetAttributeInfo("transformationType");
+            sxtPhysics.componentChild = sxtPhysics.Type.GetChildInfo("component");
+            sxtPhysics.gameObjectChild = sxtPhysics.Type.GetChildInfo("gameObject");
+
+            sxtGraphics.Type = getNodeType("gap", "sxtGraphics");
+            sxtGraphics.transformAttribute = sxtGraphics.Type.GetAttributeInfo("transform");
+            sxtGraphics.translateAttribute = sxtGraphics.Type.GetAttributeInfo("translate");
+            sxtGraphics.rotateAttribute = sxtGraphics.Type.GetAttributeInfo("rotate");
+            sxtGraphics.scaleAttribute = sxtGraphics.Type.GetAttributeInfo("scale");
+            sxtGraphics.pivotAttribute = sxtGraphics.Type.GetAttributeInfo("pivot");
+            sxtGraphics.nameAttribute = sxtGraphics.Type.GetAttributeInfo("name");
+            sxtGraphics.visibleAttribute = sxtGraphics.Type.GetAttributeInfo("visible");
+            sxtGraphics.lockedAttribute = sxtGraphics.Type.GetAttributeInfo("locked");
+            sxtGraphics.transformationTypeAttribute = sxtGraphics.Type.GetAttributeInfo("transformationType");
+            sxtGraphics.componentChild = sxtGraphics.Type.GetChildInfo("component");
+            sxtGraphics.resourceChild = sxtGraphics.Type.GetChildInfo("resource");
+            sxtGraphics.stmRefChild = sxtGraphics.Type.GetChildInfo("stmRef");
 
             locatorType.Type = getNodeType("gap", "locatorType");
             locatorType.transformAttribute = locatorType.Type.GetAttributeInfo("transform");
@@ -231,34 +256,6 @@ namespace LevelEditor
             propertyType.descriptionAttribute = propertyType.Type.GetAttributeInfo("description");
             propertyType.categoryAttribute = propertyType.Type.GetAttributeInfo("category");
             propertyType.warningAttribute = propertyType.Type.GetAttributeInfo("warning");
-
-            sxtPhysics.Type = getNodeType("gap", "sxtPhysics");
-            sxtPhysics.transformAttribute = sxtPhysics.Type.GetAttributeInfo("transform");
-            sxtPhysics.translateAttribute = sxtPhysics.Type.GetAttributeInfo("translate");
-            sxtPhysics.rotateAttribute = sxtPhysics.Type.GetAttributeInfo("rotate");
-            sxtPhysics.scaleAttribute = sxtPhysics.Type.GetAttributeInfo("scale");
-            sxtPhysics.pivotAttribute = sxtPhysics.Type.GetAttributeInfo("pivot");
-            sxtPhysics.nameAttribute = sxtPhysics.Type.GetAttributeInfo("name");
-            sxtPhysics.visibleAttribute = sxtPhysics.Type.GetAttributeInfo("visible");
-            sxtPhysics.lockedAttribute = sxtPhysics.Type.GetAttributeInfo("locked");
-            sxtPhysics.transformationTypeAttribute = sxtPhysics.Type.GetAttributeInfo("transformationType");
-            sxtPhysics.componentChild = sxtPhysics.Type.GetChildInfo("component");
-            sxtPhysics.resourceChild = sxtPhysics.Type.GetChildInfo("resource");
-            sxtPhysics.stmRefChild = sxtPhysics.Type.GetChildInfo("stmRef");
-
-            sxtGraphics.Type = getNodeType("gap", "sxtGraphics");
-            sxtGraphics.transformAttribute = sxtGraphics.Type.GetAttributeInfo("transform");
-            sxtGraphics.translateAttribute = sxtGraphics.Type.GetAttributeInfo("translate");
-            sxtGraphics.rotateAttribute = sxtGraphics.Type.GetAttributeInfo("rotate");
-            sxtGraphics.scaleAttribute = sxtGraphics.Type.GetAttributeInfo("scale");
-            sxtGraphics.pivotAttribute = sxtGraphics.Type.GetAttributeInfo("pivot");
-            sxtGraphics.nameAttribute = sxtGraphics.Type.GetAttributeInfo("name");
-            sxtGraphics.visibleAttribute = sxtGraphics.Type.GetAttributeInfo("visible");
-            sxtGraphics.lockedAttribute = sxtGraphics.Type.GetAttributeInfo("locked");
-            sxtGraphics.transformationTypeAttribute = sxtGraphics.Type.GetAttributeInfo("transformationType");
-            sxtGraphics.componentChild = sxtGraphics.Type.GetChildInfo("component");
-            sxtGraphics.resourceChild = sxtGraphics.Type.GetChildInfo("resource");
-            sxtGraphics.stmRefChild = sxtGraphics.Type.GetChildInfo("stmRef");
 
             renderComponentType.Type = getNodeType("gap", "renderComponentType");
             renderComponentType.nameAttribute = renderComponentType.Type.GetAttributeInfo("name");
@@ -884,6 +881,37 @@ namespace LevelEditor
             public static AttributeInfo lockedAttribute;
             public static AttributeInfo transformationTypeAttribute;
             public static ChildInfo componentChild;
+        }
+
+        public static class sxtPhysics
+        {
+            public static DomNodeType Type;
+            public static AttributeInfo transformAttribute;
+            public static AttributeInfo translateAttribute;
+            public static AttributeInfo rotateAttribute;
+            public static AttributeInfo scaleAttribute;
+            public static AttributeInfo pivotAttribute;
+            public static AttributeInfo nameAttribute;
+            public static AttributeInfo visibleAttribute;
+            public static AttributeInfo lockedAttribute;
+            public static AttributeInfo transformationTypeAttribute;
+            public static ChildInfo componentChild;
+            public static ChildInfo gameObjectChild;
+        }
+
+        public static class sxtGraphics
+        {
+            public static DomNodeType Type;
+            public static AttributeInfo transformAttribute;
+            public static AttributeInfo translateAttribute;
+            public static AttributeInfo rotateAttribute;
+            public static AttributeInfo scaleAttribute;
+            public static AttributeInfo pivotAttribute;
+            public static AttributeInfo nameAttribute;
+            public static AttributeInfo visibleAttribute;
+            public static AttributeInfo lockedAttribute;
+            public static AttributeInfo transformationTypeAttribute;
+            public static ChildInfo componentChild;
             public static ChildInfo resourceChild;
             public static ChildInfo stmRefChild;
         }
@@ -939,40 +967,6 @@ namespace LevelEditor
             public static AttributeInfo descriptionAttribute;
             public static AttributeInfo categoryAttribute;
             public static AttributeInfo warningAttribute;
-        }
-
-        public static class sxtPhysics
-        {
-            public static DomNodeType Type;
-            public static AttributeInfo transformAttribute;
-            public static AttributeInfo translateAttribute;
-            public static AttributeInfo rotateAttribute;
-            public static AttributeInfo scaleAttribute;
-            public static AttributeInfo pivotAttribute;
-            public static AttributeInfo nameAttribute;
-            public static AttributeInfo visibleAttribute;
-            public static AttributeInfo lockedAttribute;
-            public static AttributeInfo transformationTypeAttribute;
-            public static ChildInfo componentChild;
-            public static ChildInfo resourceChild;
-            public static ChildInfo stmRefChild;
-        }
-
-        public static class sxtGraphics
-        {
-            public static DomNodeType Type;
-            public static AttributeInfo transformAttribute;
-            public static AttributeInfo translateAttribute;
-            public static AttributeInfo rotateAttribute;
-            public static AttributeInfo scaleAttribute;
-            public static AttributeInfo pivotAttribute;
-            public static AttributeInfo nameAttribute;
-            public static AttributeInfo visibleAttribute;
-            public static AttributeInfo lockedAttribute;
-            public static AttributeInfo transformationTypeAttribute;
-            public static ChildInfo componentChild;
-            public static ChildInfo resourceChild;
-            public static ChildInfo stmRefChild;
         }
 
         public static class renderComponentType
